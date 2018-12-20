8e0169a8021d514f20d61abd10c48d0998b7d81a
Andrey <andrey64freemail@gmail.com>
2018-12-20 15:29:12 +0200

    date_instead_hello

--- index.php
+++ index.php
@@ -4,13 +4,17 @@ $file_db->exec("create table if not exists test(
     id INTEGER PRIMARY KEY,
     title TEXT)");
 
-$var = $file_db->exec("insert into test (title) values ('hello')");
-echo $var;
+$timehello = date('Y-m-d'.' '.'G:i:s');
+$var = $file_db->exec("insert into test (title) values ('$timehello')");
+echo $var. "<br>";
 
 $res = $file_db->query("select * from test");
 /*print_r ($res->fetchAll());*/
 
 echo "<table>";
+
+$date = new DateTime();
+echo $date->getTimestamp();
 
 foreach($res as $key => $value)
 {
